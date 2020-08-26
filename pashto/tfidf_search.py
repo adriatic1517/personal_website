28c94acde818ad0493e81f8e90987300635d5224
adriatic1517 <emadsiddiq@berkeley.edu>
2020-08-25 23:47:36 -0700

    a

--- pashto/tfidf_search.py
+++ pashto/tfidf_search.py
@@ -16,11 +16,15 @@ def search(df,column, word, threshold = 0.5, top=10):
     tf_idf_matrix_dirty = vectorizer.transform(pd.Series([word])) #transform data on input
     similarity = cosine_similarity(tf_idf_matrix_clean, tf_idf_matrix_dirty) #get cosine_similarity between two 
     similarity = pd.Series([i[0] for i in similarity]) #data_cleaning
+
     ans = pd.concat([df[column].reset_index(drop=True), similarity], axis=1).sort_values(by=0, ascending=False) #join matrices
     ans.columns = ['word', 'similarity'] 
+
     ans = ans[ans['similarity']>threshold]
     ans = ans.sort_values(by='similarity', ascending=False)[:top]
     print(ans)
     suggestions = ans['word'].to_list()
-    return suggestions
+    if len(suggestions) >0:
+        return suggestions
+    return []
 
