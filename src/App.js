5f593f53f6b8e7066c2a419bc9f6238ffe26cb7c
adriatic1517 <emadsiddiq@berkeley.edu>
2021-09-24 08:31:29 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import React from 'react';
 
+
 export default class App extends React.Component {
   render() {
     return (
       <div className="Portfolio">
         <Name />
+        <Portrait/>
         <AboutMe />
         <Coursework />
         <Projects/>
@@ -27,6 +29,15 @@ class Name extends React.Component {
   }
 }
 
+class Portrait extends React.Component{
+  render()
+ {
+   return (
+     <div className="Portrait">
+       <img src="/portrait.jpg"></img>
+     </div>
+   )
+ }}
 
 
 class AboutMe extends React.Component {
@@ -37,8 +48,8 @@ class AboutMe extends React.Component {
         <p>Hello! I am Emad. 
           I recently graduated from UC Berkeley with a Bachelors in Political Economy & Data Science. </p>
 
-        <p> Quick links:<br /><a href="https://www.ocf.berkeley.edu/~emadsiddiq/run.fcgi/resume/static/data/Emad_Resume.pdf"
-          >Resume</a> and <a href="https://www.linkedin.com/in/emad-siddiq-2b9979164/"> LinkedIn</a></p>
+        <p> Quick links:<br /><a href="/resume.pdf">Resume</a>
+ and <a href="https://www.linkedin.com/in/emad-siddiq-2b9979164/"> LinkedIn</a></p>
 
     </div>
     );
@@ -52,8 +63,11 @@ class Coursework extends React.Component {
     return (
       <div className="Coursework" id="Coursework">
         <h2>100 Days of AI</h2>
+        <p> I will attempt to do a 100 data science explorations in 100 days. Starting
+          July 27th 2021.
+        </p>
         <ul>
-          <li> <a href = "http://emad-resume.herokuapp.com/day1.html">Day 1</a></li>
+          <li> <a href = "http://emad-resume.herokuapp.com/day1.html">Day 1: July 27 2021</a></li>
         </ul>
       </div>
     );
@@ -66,19 +80,18 @@ class Projects extends React.Component {
       <div className="Projects" id = "Projects">
         <h1>Projects</h1>
         <a href = "https://ocf.berkeley.edu/~emadsiddiq/goodnews"><h3>Good News</h3></a>
-        <p>This project was born out of a desire to give my friends and family a positive outlook when there was nothing
-          but upsetting pieces on newspapers main pages. Therefore, I used Sentiment Analysis on major news sources and gathered the
+        <p> In the first quarantine, I used Sentiment Analysis on major news sources and gathered the
           positive articles from within on one website for my friends to view. </p>
 
         <a href = "https://ocf.berkeley.edu/~emadsiddiq/pashto"><h3>Dictionary of Pashto</h3></a>
         <p>This project  a one of a kind dictionary of my mother tongue Pashto was born out of a greater objective of
-           digitizing Pashto language and literature. I see this project as a start of creating more products that can
-            bridge the linguistic gap between the millions in the non-English speaking parts of the world and help 
-            bring us closer together through literature.</p>
+           digitizing Pashto language and literature.</p>
       </div>
     );
   }
 }
+
+
 
 
 
