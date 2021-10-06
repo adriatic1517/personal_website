1c0b1bdbfd14a8bfeefb29cb2eb22fedfa00a266
adriatic1517 <emadsiddiq@berkeley.edu>
2021-10-05 19:28:29 -0700

    resume website updated

--- src/App.js
+++ src/App.js
@@ -7,10 +7,21 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="Portfolio">
-        <Name/>
+   
         <Portrait/>
-        <AboutMe />
+        <Name/>
+        <AboutMe/>
   
+      </div>
+    );
+  }
+}
+
+class Portrait extends React.Component {
+  render(){
+    return (
+      <div className="Portrait">
+         <img src="/portrait.jpg"></img>
       </div>
     );
   }
@@ -27,15 +38,7 @@ class Name extends React.Component {
   }
 }
 
-class Portrait extends React.Component{
-  render()
- {
-   return (
-     <div className="Portrait">
-       <img src="/portrait.jpg"></img>
-     </div>
-   )
- }}
+
 
 
 
@@ -44,19 +47,28 @@ class AboutMe extends React.Component {
   render(){
     return (
       <div className="AboutMe" id = "About Me">
+        <p className= "Links">
+           <a href="/resume.pdf">Resume</a> |
+           <a href="https://www.linkedin.com/in/emad-siddiq-2b9979164/"> LinkedIn </a> |
+           <a href="https://github.com/adriatic1517"> Github</a>
+            </p>
+        
+        <p className="hello">Hello and welcome! &#128075;</p>
+        
+        <p>I recently graduated from UC Berkeley with a Bachelors in Political Economy & Data Science. I am actively looking for entry level engineering positions.
+        </p>
+        <p> My academic interests center around AI and the ML models that power them.
+         Currently I am reading on how CNN's can be used for OCR of languages in the Persio-Arabic script.</p>
+        
+        <p>Inspired by Professor Michael Jordans's work on Machine Learning and Micreconomic Theory, 
+          I am driven to <a href="">understand</a> how AI can bring archaic financial and economic institutions into the 21st century.
+        </p>
 
-        <p>Hello! I am Emad. I graduated this May from UC Berkeley with a Bachelors in Political Economy & Data Science.</p>
-        <p> I am interested in Artificial Intelligence and the Machine Learning models that power them.</p>
-        <p> Currently I am reading on how CNN's can be used for OCR of languages in the Persio-Arabic script</p>
+        <p>I look forward to connecting with you!</p>
 
-
-        <p> Quick links:<br /><a href="/resume.pdf">Resume</a>
- and <a href="https://www.linkedin.com/in/emad-siddiq-2b9979164/"> LinkedIn</a></p>
 
     </div>
     );
-
-
   }
 }
 
@@ -65,8 +77,7 @@ class Coursework extends React.Component {
     return (
       <div className="Coursework" id="Coursework">
         <h2>100 Days of AI</h2>
-        <p> I will attempt to do a 100 data scienc
-        </p>
+  
         <ul>
           <li> <a href = "http://emad-resume.herokuapp.com/day1.html">Day 1: July 27 2021</a></li>
         </ul>
@@ -79,17 +90,17 @@ class Projects extends React.Component {
   render() {
     return (
       <div className="Projects" id = "Projects">
-        <h1>Projects</h1>
-        <a href = "https://ocf.berkeley.edu/~emadsiddiq/goodnews"><h3>Good News</h3></a>
-        <p> In the first quarantine, I used Sentiment Analysis on major news sources and gathered the
-          positive articles from within on one website for my friends to view. </p>
+        <h1> Research</h1>
+        <p>Conducted research for Professor Sonya Katyal’s Trademark & Branding Project as the team’s lead data scientist
+</p>
 
-        <a href = "https://ocf.berkeley.edu/~emadsiddiq/pashto"><h3>Dictionary of Pashto</h3></a>
-        <p>This project  a one of a kind dictionary of my mother tongue Pashto was born out of a greater objective of
-           digitizing Pashto language and literature.</p>
-          
-        <a href = "https://colab.research.google.com/drive/1jVD2GgqDi_ImgGwkXQ8wim1AIsopVD-R?usp=sharing"><h3>NetworkX</h3></a>
-        <p>This was a mini-Project for my Advanced Algorithms class.</p>
+        <p>Parallelized data extraction from remote XML files, with limited cloud storage (5 GB), for over 100GB of data
+</p>
+
+        <p>Designed a fast matching algorithm for relevant companies from a list provided by Haas School of Business
+</p>
+        
+        
 
 
 
