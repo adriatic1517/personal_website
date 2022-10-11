43f4dc39e2613b816c1be4089baaf280a0415737
Emad Siddiq <emadsiddiq@berkeley.edu>
2022-10-10 21:53:27 -0700

    home page

---
+++ pages/blog.js
@@ -0,0 +1,82 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Emad's Blog</title>
+        <meta name="description" content="Fun stuff from CS" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Title/>
+      <Blogs allPostsData={allPostsData}/>
+      </div>
+  )
+}
+
+function Blogs({allPostsData}) {
+  return (
+    <div className={styles.Blogs}>
+      {allPostsData.map(({ id, date, title, bg_image}) => (
+      <BlogPost css = "Blog" title = {title} id = {id} date= {date} key={id} bg_image = {bg_image}/>
+    ))}
+    </div>
+  )
+}
+
+function Title() {
+  return (
+    <div className={styles.Title}>
+      
+      <h1>Emad's Blog</h1> 
+      <p>Very fancy</p>
+    </div> 
+  )
+}
+
+// 
+
+function BlogPost(props) {
+  
+  return (
+    <Link href = {"/posts/" + props.id}>
+    <div className={styles[props.css]} style={{width: '100%', position: 'relative'}}>
+     
+     <Image src={props.bg_image} layout='fill'/>
+    <PostDetails {...props}/>
+
+    </div>
+    </Link>
+
+
+  )
+}
+
+
+
+
+function PostDetails(props) {
+  return (
+    <div className={styles.PostDetails}>
+      <h1> {props.title.slice(0, 50)} </h1>
+      <p>{props.date}</p> 
+    </div>
+
+  )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,8 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
-import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-export default function Home({ allPostsData }) {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -23,60 +13,7 @@ export default function Home({ allPostsData }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Title/>
-      <Blogs allPostsData={allPostsData}/>
+      <h1>Hello</h1>
       </div>
   )
 }
-
-function Blogs({allPostsData}) {
-  return (
-    <div className={styles.Blogs}>
-      {allPostsData.map(({ id, date, title, bg_image}) => (
-      <BlogPost css = "Blog" title = {title} id = {id} date= {date} key={id} bg_image = {bg_image}/>
-    ))}
-    </div>
-  )
-}
-
-function Title() {
-  return (
-    <div className={styles.Title}>
-      
-      <h1>Emad's Blog</h1> 
-      <p>Very fancy</p>
-    </div> 
-  )
-}
-
-// 
-
-function BlogPost(props) {
-  
-  return (
-    <Link href = {"/posts/" + props.id}>
-    <div className={styles[props.css]} style={{width: '100%', position: 'relative'}}>
-     
-     <Image src={props.bg_image} layout='fill'/>
-    <PostDetails {...props}/>
-
-    </div>
-    </Link>
-
-
-  )
-}
-
-
-
-
-function PostDetails(props) {
-  return (
-    <div className={styles.PostDetails}>
-      <h1> {props.title.slice(0, 50)} </h1>
-      <p>{props.date}</p> 
-    </div>
-
-  )
-}
-
