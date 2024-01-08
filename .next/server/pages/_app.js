27022e8d8034e41cca261994a2a9f2da139e5b2e
Emad Siddiq <emadsiddiq@Emads-MacBook.local>
2024-01-08 13:38:33 -0800

    fix css for mobile

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,45 +1,56 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
 (() => {
 var exports = {};
-exports.id = 888;
-exports.ids = [888];
+exports.id = "pages/_app";
+exports.ids = ["pages/_app"];
 exports.modules = {
 
-/***/ 8510:
+/***/ "./pages/_app.js":
+/*!***********************!*\
+  !*** ./pages/_app.js ***!
+  \***********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (/* binding */ App)
-/* harmony export */ });
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var highlight_js_styles_github_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5062);
-/* harmony import */ var highlight_js_styles_github_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(highlight_js_styles_github_css__WEBPACK_IMPORTED_MODULE_1__);
-
-
-
-function App({ Component , pageProps  }) {
-    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
-        ...pageProps
-    });
-};
-
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ App)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _styles_global_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles/global.css */ \"./styles/global.css\");\n/* harmony import */ var _styles_global_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_global_css__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var highlight_js_styles_github_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! highlight.js/styles/github.css */ \"./node_modules/highlight.js/styles/github.css\");\n/* harmony import */ var highlight_js_styles_github_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(highlight_js_styles_github_css__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\nfunction App({ Component , pageProps  }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n        ...pageProps\n    }, void 0, false, {\n        fileName: \"/Users/emadsiddiq/Projects/personal_website/pages/_app.js\",\n        lineNumber: 5,\n        columnNumber: 10\n    }, this);\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9fYXBwLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQTtBQUE4QjtBQUNVO0FBRXpCLFNBQVNBLEdBQUcsQ0FBQyxFQUFFQyxTQUFTLEdBQUVDLFNBQVMsR0FBRSxFQUFFO0lBQ3BELHFCQUFPLDhEQUFDRCxTQUFTO1FBQUUsR0FBR0MsU0FBUzs7Ozs7WUFBSSxDQUFDO0NBQ3JDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbmV3X3Jlc3VtZS8uL3BhZ2VzL19hcHAuanM/ZTBhZCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgXCIuLi9zdHlsZXMvZ2xvYmFsLmNzc1wiO1xuaW1wb3J0IFwiaGlnaGxpZ2h0LmpzL3N0eWxlcy9naXRodWIuY3NzXCI7XG5cbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIEFwcCh7IENvbXBvbmVudCwgcGFnZVByb3BzIH0pIHtcbiAgcmV0dXJuIDxDb21wb25lbnQgey4uLnBhZ2VQcm9wc30gLz47XG59XG4iXSwibmFtZXMiOlsiQXBwIiwiQ29tcG9uZW50IiwicGFnZVByb3BzIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/_app.js\n");
 
 /***/ }),
 
-/***/ 5062:
+/***/ "./node_modules/highlight.js/styles/github.css":
+/*!*****************************************************!*\
+  !*** ./node_modules/highlight.js/styles/github.css ***!
+  \*****************************************************/
 /***/ (() => {
 
 
 
 /***/ }),
 
-/***/ 997:
+/***/ "./styles/global.css":
+/*!***************************!*\
+  !*** ./styles/global.css ***!
+  \***************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("react/jsx-runtime");
+module.exports = require("react/jsx-dev-runtime");
 
 /***/ })
 
@@ -50,7 +61,7 @@ module.exports = require("react/jsx-runtime");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(8510));
+var __webpack_exports__ = (__webpack_exec__("./pages/_app.js"));
 module.exports = __webpack_exports__;
 
 })();
--- components/homepage/blogpost.js
+++ components/homepage/blogpost.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 
 
@@ -7,11 +6,34 @@ export default function BlogPost(props) {
     return (
       <Link href={"/posts/" + props.id}>
         <div
-          className={styles[props.css]}
+          className="Blog"
           style={{ width: "100%", position: "relative" }}
         >
           <Image src={props.bg_image} layout="fill" />
           <PostDetails {...props} />
+          <style jsx>
+          {
+            `
+            @media (min-width: 700px) {
+
+              .Blog {
+                height:50vh;
+                margin-bottom: 5vh;
+              }
+              
+            }
+            
+            @media (max-width: 700px) {
+
+              .Blog {
+                height: 32vh;
+                margin-bottom: 5vh;
+              }
+            }
+            `
+
+              }
+          </style>
         </div>
       </Link>
     );
@@ -20,11 +42,76 @@ export default function BlogPost(props) {
 
 function PostDetails(props) {
     return (
-      <div className={styles.PostDetails}>
-        <h1 className={styles.postTitle}> {props.title.slice(0, 50)} </h1>
-        <p className={styles.postDate}>{props.date}</p>
+      <div className="PostDetails">
+        <h1 className="postTitle"> {props.title.slice(0, 50)} </h1>
+        <p className="postDate">{props.date}</p>
+
+        <style jsx>
+          {
+            `
+            @media (min-width: 700px) {
+              .PostDetails {
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                color: white;
+                background-color: rgba(45, 45, 45, 0.7);
+                width: 95%;
+                padding-left: 5%;
+                height: 10vh;
+            
+                overflow: hidden;
+              }
+              .postTitle {
+                font-size: 24px;
+                color: white;
+                font-weight: 600;
+                margin-bottom: -1vh;
+              }
+              .postDate {
+                font-weight: 400;
+                font-size: 16px;
+                margin-left: 0.25vw;
+              }
+
+            }
+
+              
+            @media (max-width: 700px) {
+
+             
+              
+              .PostDetails {
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                color: white;
+                background-color: rgb(0, 0, 0, 0.8);
+                width: 95%;
+                padding-left: 5%;
+                height: 30%;
+                line-height: 60%;
+            
+                overflow: hidden;
+              }
+              .PostDetails > h1 {
+                font-size: 2.6vh;
+                font-weight: 500;
+              }
+              .PostDetails > p {
+                font-size: 2vh;
+                font-weight: 400;
+              }
+            
+            }       
+            
+            `
+          }
+        </style>
       </div>
     );
   }
+
+  
   
   
