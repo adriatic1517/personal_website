977b7be5f052fcad60cc4dc9af91c76c4519beff
Emad Siddiq <emadsiddiq@Emads-MacBook.local>
2024-01-31 09:00:46 -0800

    fix iphone blog

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a352a8fe294bed3e"; }
+/******/ 		__webpack_require__.h = function() { return "1a537841f0f17835"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.6f258cb76342ce0d.hot-update.js
+++ .next/static/webpack/webpack.6f258cb76342ce0d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "57faa03361ac1643"; }
+/******/ 	__webpack_require__.h = function() { return "d779fba1c5be39a6"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
